74cf5f67ceb338e6b260fef3405eaae9bbf4bf2e
Juan Carlos González <juancarlos.gonzalez@orbitaleap.com>
2026-07-24 07:58:27 +0200

    fix(deploy): remove .wrangler/ state from git tracking & ignore local miniflare state

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: false,
+    },
+  }),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
